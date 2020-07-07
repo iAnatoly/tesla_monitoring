49aca5b07adfeeb4b2f158157c14d1bcb38d04e2
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2020-07-07 11:46:28 -0700

    dumping into influx

--- tesla-influx.py
+++ tesla-influx.py
@@ -12,28 +12,35 @@ import json
 import time
 from datetime import datetime
 from tesla_api import TeslaApiClient
-from influxdb import InfluxDBClient
+#from influxdb import InfluxDBClient
+from aioinflux import InfluxDBClient
 
 
 def log(msg):
     print(msg)
 
 async def main():
-    with open('/etc/homebridge/hbr-tesla/config.json') as conf_file:
+    with open('tesla-influx.conf.json') as conf_file:
         config = json.load(conf_file)
+        assert config
 
-    #try:
-        target_vin = config["accessories"][0]['vin']
+    with open(config['homebridge_conf_path']) as hbr_conf_file:
+        hbr_config = json.load(hbr_conf_file)
+        assert hbr_config
+
+    try:
+        timestamp = int(time.time()-1)
+        target_vin = hbr_config["accessories"][0]['vin']
+        # mock a token structure to match the target format
         token = {
-                "access_token": config["accessories"][0]["authToken"],
-                "expires_in": 3888000,
-                "created_at": int(time.time()-1),
-                "refresh_token": ""
+                "access_token": hbr_config["accessories"][0]["authToken"],
+                "expires_in": 3888000,      # does not matter
+                "created_at": timestamp,    # just now
+                "refresh_token": ""         # does not matter
         }
         client = TeslaApiClient(token=token)
 
         vehicles = await client.list_vehicles()
-        log(vehicles)
         
         vehicle = [ vehicle for vehicle in vehicles if vehicle.vin == target_vin ][0]
 
@@ -49,16 +56,40 @@ async def main():
         drive_state = await vehicle.get_drive_state()
         charge_state = await vehicle.charge.get_state()
         
-        print(json.dumps(charge_state, indent=4))
+        json_body = [
+            {
+                'measurement': 'charge_state',
+        '        tags': {
+                    'vin': target_vin,
+                },
+                'time': timestamp,
+                'fields': charge_state,
+            },
+            {
+                'measurement': 'drive_state',
+        '        tags': {
+                    'vin': target_vin,
+                },
+                'time': timestamp,
+                'fields': drive_state,
+            }
+        ]
+        # TODO: place this in local conf
+        influx_config=config['influx']
+        assert influx_config 
 
-        # TODO: dump json into influx
+        async with InfluxDBClient(
+                host=influx_config['host'], 
+                port=influx_config['port'], 
+                username=influx_config['username'],
+                password=influx_config['password'],
+                db=influx_config['db']) as influx_client:
+            await influx_client.write(json_body)
 
-    try:
-        pass
     except Exception as ex:
         log(ex)
     finally:
         await client.close()
-
+        
 if __name__=='__main__':
     asyncio.run(main())
