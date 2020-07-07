79b17309114055dce66e33b47494bc934f180205
Anatoly Ivanov <anatoly.ivanov@gmail.com>
2020-07-07 11:01:18 -0700

    auth is working

--- tesla-influx.py
+++ tesla-influx.py
@@ -12,36 +12,49 @@ import json
 import time
 from datetime import datetime
 from tesla_api import TeslaApiClient
+from influxdb import InfluxDBClient
 
+
+def log(msg):
+    print(msg)
 
 async def main():
-    with open('tesla-monitoring.conf.json') as conf_file:
+    with open('/etc/homebridge/hbr-tesla/config.json') as conf_file:
         config = json.load(conf_file)
 
-    try:
-        # TODO: figure out the json path
-        client = TeslaApiClient(token=config["token"])
+    #try:
+        target_vin = config["accessories"][0]['vin']
+        token = {
+                "access_token": config["accessories"][0]["authToken"],
+                "expires_in": 3888000,
+                "created_at": int(time.time()-1),
+                "refresh_token": ""
+        }
+        client = TeslaApiClient(token=token)
 
         vehicles = await client.list_vehicles()
-        # TODO: use VIN from config
-        vehicle = [ vehicle for vehicle in vehicles if vehicle.display_name == config["vehicle_name"] ][0]
+        log(vehicles)
+        
+        vehicle = [ vehicle for vehicle in vehicles if vehicle.vin == target_vin ][0]
+
         if vehicle is None:
-            raise VehicleNotFoundException('Vehicle {} not found'.format(config["vehicle_name"]))
+            raise VehicleNotFoundException('Vehicle {} not found'.format(vin))
 
         # if vehicle is offline, check if any of the schedules allow waking up. Wake up or quit
         if vehicle.state != 'online':
-            raise VehicleOfflineException("Vehicle is offline, and none of the schedules allow wake up")
+            raise Exception("Vehicle is offline. That is OK.")
 
 
         # TODO: maybe grab the whole config?
         drive_state = await vehicle.get_drive_state()
         charge_state = await vehicle.charge.get_state()
         
-        # print(json.dumps(charge_state, indent=4))
+        print(json.dumps(charge_state, indent=4))
 
         # TODO: dump json into influx
 
-
+    try:
+        pass
     except Exception as ex:
         log(ex)
     finally:
